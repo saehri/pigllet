370c87cd35ca036e2cdbbb51c2d1aa52754e3321
Saepul Bahri <bahreesaepul1@gmail.com>
2025-03-04 02:33:20 +0700

    refactor: update font styles and improve item titles in profile settings

--- src/components/profile/main-settings.tsx
+++ src/components/profile/main-settings.tsx
@@ -11,36 +11,48 @@ export default function MainSetting() {
 			<List.Item
 				title="Account"
 				description="Avatar, email, password"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => <User {...props} size={24} strokeWidth={1.5} color={props.color} />}
 			/>
 
 			<List.Item
 				title="Transaction category"
 				description="Income, expense, transfer"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => (
 					<Notebook {...props} size={24} strokeWidth={1.5} color={props.color} />
 				)}
 			/>
 
 			<List.Item
-				title="Cloud Sync"
+				title="Cloud Sync (Beta)"
 				description="You are up to date"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => <CloudUpload {...props} size={24} strokeWidth={1.5} color={props.color} />}
 			/>
 
 			<List.Item
 				title="Security"
 				description="App lock, fingerprint"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => <LockKeyhole {...props} size={24} strokeWidth={1.5} color={props.color} />}
 			/>
 
 			<List.Item
 				title="Wallets"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => <Wallet2 {...props} size={24} strokeWidth={1.5} color={props.color} />}
 			/>
 
 			<List.Item
 				title="Currency"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => <CurrencyIcon {...props} size={24} strokeWidth={1.5} color={props.color} />}
 			/>
 		</View>
--- src/components/profile/secondary-setting.tsx
+++ src/components/profile/secondary-setting.tsx
@@ -4,11 +4,13 @@ import { BadgeInfo, FlaskRound, Heart, Mail, Star, User } from 'lucide-react-nat
 export default function SecondarySetting() {
 	return (
 		<List.Section>
-			<List.Subheader>Billing and support</List.Subheader>
+			<List.Subheader style={{ fontFamily: 'Inter-Black' }}>Billing and support</List.Subheader>
 
 			<List.Item
-				title="Premium"
+				title="Premium (Coming soon)"
 				description="Get the premium version of the app in Play Store"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => (
 					<Star style={{ ...props.style, alignSelf: 'center' }} size={24} strokeWidth={1.5} color={props.color} />
 				)}
@@ -17,6 +19,8 @@ export default function SecondarySetting() {
 			<List.Item
 				title="Write a review"
 				description="if you are enjoying Pigllet please leave a review on the Play Store"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => (
 					<Heart
 						{...props}
@@ -29,12 +33,16 @@ export default function SecondarySetting() {
 			<List.Item
 				title="Contact us"
 				description="If you need help or have some advice"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => <Mail {...props} size={24} strokeWidth={1.5} color={props.color} />}
 			/>
 
 			<List.Item
 				title="Become a tester"
 				description="Access to early builds to get new feature faster"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => (
 					<FlaskRound
 						{...props}
@@ -46,6 +54,8 @@ export default function SecondarySetting() {
 
 			<List.Item
 				title="About"
+				titleStyle={{ fontFamily: 'Inter-Regular' }}
+				descriptionStyle={{ fontFamily: 'Inter-Light' }}
 				left={(props) => <BadgeInfo {...props} size={24} strokeWidth={1.5} color={props.color} />}
 			/>
 		</List.Section>
