c08610f6f2390e6f7b5e7769ad5576659214090f
saehri <bahreesaepul1@gmail.com>
2025-03-25 10:39:26 +0700

    feat: refactor TransfersScreen to use ScrollView and display progress message

--- src/app/(root)/(tabs)/transactions/transfer.tsx
+++ src/app/(root)/(tabs)/transactions/transfer.tsx
@@ -1,55 +1,45 @@
-import { useState } from 'react';
-import { FlatList } from 'react-native';
-import { useTheme } from 'react-native-paper';
-import { useSQLiteContext } from 'expo-sqlite';
-import { drizzle, useLiveQuery } from 'drizzle-orm/expo-sqlite';
-import { and, eq } from 'drizzle-orm';
+import { ScrollView, View } from 'react-native';
+import { Text, useTheme } from 'react-native-paper';
 
-import * as schema from '@/db/schema';
-
-import NoItemNotice from '@/src/components/reusables/no-items-notice';
-import TransferCard from '@/src/components/reusables/transfer-card';
+import { Workflow } from 'lucide-react-native';
 
 export default function TransfersScreen() {
 	const theme = useTheme();
-	const [date] = useState(new Date());
-
-	const db = useSQLiteContext();
-	const drizzleDb = drizzle(db, { schema });
-
-	const { data } = useLiveQuery(
-		drizzleDb
-			.select()
-			.from(schema.transactions)
-			.where(
-				and(
-					eq(schema.transactions.created_month, date.getMonth()),
-					eq(schema.transactions.created_year, date.getFullYear())
-				)
-			)
-			.innerJoin(
-				schema.categories,
-				eq(schema.transactions.category_id, schema.categories.id)
-			)
-			.innerJoin(
-				schema.accounts,
-				eq(schema.transactions.related_account_id, schema.accounts.id)
-			)
-	);
 
 	return (
-		<FlatList
-			style={{ paddingTop: 60, backgroundColor: theme.colors.background }}
-			data={data}
-			ListEmptyComponent={<NoItemNotice />}
-			renderItem={({ item }) => (
-				<TransferCard
-					key={item.transactions.id}
-					data={item.transactions}
-					account={item.accounts}
-					category={item.categories}
+		<ScrollView
+			showsVerticalScrollIndicator={false}
+			style={{
+				backgroundColor: theme.colors.background,
+			}}
+		>
+			<View
+				style={{
+					alignItems: 'center',
+					justifyContent: 'center',
+					padding: 16,
+					paddingTop: 59,
+				}}
+			>
+				<Workflow
+					size={120}
+					strokeWidth={0.5}
+					fillOpacity={0.3}
+					fill={theme.colors.primary}
+					color={theme.colors.onBackground}
 				/>
-			)}
-		/>
+				<Text
+					style={{
+						fontFamily: 'Inter-Regular',
+						textAlign: 'center',
+						maxWidth: 280,
+					}}
+					variant="bodyLarge"
+				>
+					This feature is in progress, and we’re working hard to deliver it
+					soon.
+				</Text>
+			</View>
+		</ScrollView>
 	);
 }
